246b477239f07f357de2c88764f1b126423261bc
f <839322915@qq.com>
2018-06-03 21:05:26 +0800

    update

--- src/main/java/com/f/dao/GenericCrudMapper.java
+++ src/main/java/com/f/dao/GenericCrudMapper.java
@@ -1,9 +1,19 @@
 package com.f.dao;
 
+import org.apache.ibatis.annotations.Param;
+
 import java.io.Serializable;
+import java.util.List;
 
 public interface GenericCrudMapper<T, ID extends Serializable> {
     Object fetchById(ID id);
 
-    int insert(T t);
+    //List<T> fetch(@Param("searchCriteria") SearchCriteria searchCriteria); 封装一个查询累用来判断查询
+    List<T> fetch(@Param("searchCriteria") String searchCriteria);
+
+    int insert(T o);
+
+    void update(T o);
+
+    void delete(T o);
 }
