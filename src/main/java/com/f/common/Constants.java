879d26083558ccedda5a7c13b2ecd90a44336900
f <839322915@qq.com>
2018-06-08 21:58:29 +0800

    update

--- src/main/java/com/f/common/Constants.java
+++ src/main/java/com/f/common/Constants.java
@@ -6,6 +6,6 @@ import com.f.helper.OutputJsonHelper;
 public class Constants {
     public static final String CURRENT_USER = "CURRENT_USER";
 
-    private OutputJsonHelper outputJsonHelper = OutputJsonHelper.getJsonOutputInstance();
+    public static final OutputJsonHelper outputJsonHelper = OutputJsonHelper.getJsonOutputInstance();
 
 }
