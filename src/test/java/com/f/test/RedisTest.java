da5f0fa908eaa2abafdfe845dcac5bc17c9609f1
f <839322915@qq.com>
2018-06-19 23:14:42 +0800

    add test

---
+++ src/test/java/com/f/test/RedisTest.java
@@ -0,0 +1,33 @@
+package com.f.test;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.data.redis.core.ListOperations;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import javax.annotation.Resource;
+import java.net.URL;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(locations = {"classpath:applicationContext.xml"})
+public class RedisTest {
+    @Autowired
+    private RedisTemplate<String, String> template;
+
+    @Autowired
+    private StringRedisTemplate redisTemplate;
+
+    @Test
+    void getRedisTemplate() {
+        assert template != null;
+        assert redisTemplate != null;
+    }
+
+}
