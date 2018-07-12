a9e716be1ea46f28bcec0758af2ec3288916d44a
f <839322915@qq.com>
2018-07-12 23:03:27 +0800

    update quartz thread pool size, add Dict API

--- src/main/java/com/f/api/DictController.java
+++ src/main/java/com/f/api/DictController.java
@@ -1,7 +1,16 @@
 package com.f.api;
 
 import com.f.controller.BaseController;
+import com.f.core.pojo.Dict;
+import com.f.services.impl.AbstractGenericService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.RestController;
 
-public class DictController {
-
+@RestController
+public class DictController extends BaseController<Dict, Long> {
+    @Autowired
+    public DictController(@Qualifier(value = "dictService") AbstractGenericService abstractGenericService) {
+        super(abstractGenericService);
+    }
 }
